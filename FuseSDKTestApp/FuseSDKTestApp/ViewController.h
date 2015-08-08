8433ac4088d12b482d9c6c992922aeb835c9e141
Robert Segal <rob@getsetgames.com>
2015-08-08 13:38:52 -0400

    Initial commit

---
+++ FuseSDKTestApp/FuseSDKTestApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FuseSDKTestApp
+//
+//  Created by Robert Segal on 2015-08-08.
+//  Copyright (c) 2015 Get Set Games Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FuseSDKTestAppTests/FuseSDKTestAppTests.m
@@ -0,0 +1,40 @@
+//
+//  FuseSDKTestAppTests.m
+//  FuseSDKTestAppTests
+//
+//  Created by Robert Segal on 2015-08-08.
+//  Copyright (c) 2015 Get Set Games Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface FuseSDKTestAppTests : XCTestCase
+
+@end
+
+@implementation FuseSDKTestAppTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
